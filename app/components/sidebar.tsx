f9099919116c9468bfc0343eb5767047d26ba661
ydideh810 <yd.baller810@gmail.com>
2024-06-13 16:01:30 -0400

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -6,7 +6,7 @@ import { IconButton } from "./button";
 import SettingsIcon from "../icons/gear.svg";
 import GithubIcon from "../icons/github.svg";
 import InternetIcon from "../icons/internet.svg";
-import MlcIcon from "../icons/smal.svg";
+import MlcIcon from "../icons/smol.svg";
 import AddIcon from "../icons/add.svg";
 import DeleteIcon from "../icons/delete.svg";
 import TemplateIcon from "../icons/chat.svg";
