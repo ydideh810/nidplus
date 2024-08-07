9611802f275ee435193bc84d14eedf14a7b6e7c3
ydideh810 <yd.baller810@gmail.com>
2024-08-06 23:32:37 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-"use client";
+
 
 import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { type Metadata } from "next";
-import { useState } from "react";
+
 
 export const metadata: Metadata = {
   title: "Niddam_AI",
@@ -83,33 +83,6 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
-        {showDialog && (
-          <dialog
-            className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50"
-          >
-            <article className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
-              <h4 className="mb-4">Welcome to CosmoSpeak</h4>
-
-              <p className="mb-8">
-                Explore the wonders of Space Engineering through A.I.
-              </p>
-
-              <ol className="list-decimal mx-6 pl-2 mb-8">
-                <li>Get started w/ a free trial!</li>  
-              </ol>
-
-              <button
-                className="p-3 px-6 bg-black text-red-700"
-                onClick={() => {
-                  window.location.href = 'https://square.link/u/RmVSmy9L';
-                  return false;
-                }}
-              >
-                Start Today
-              </button>
-            </article>
-          </dialog>
-        )}
         {children}
       </body>
     </html>
