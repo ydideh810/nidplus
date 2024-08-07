1500fd0147e0959842aebbfd29ff82a3cca199b5
ydideh810 <yd.baller810@gmail.com>
2024-08-07 12:32:22 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./styles/globals.scss";
 import "./styles/markdown.scss";
 import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { metadata } from './metadata'
 import { Metadata } from 'next';
 // Metadata export
@@ -34,22 +34,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [showPopup, setShowPopup] = useState(true);
-  useEffect(() => {
-    // Optionally, you can use localStorage to manage the popup visibility
-    // Example: Check if the popup has been shown before
-    const hasShownPopup = localStorage.getItem('hasShownPopup');
-    if (hasShownPopup) {
-      setShowPopup(false);
-    } else {
-      localStorage.setItem('hasShownPopup', 'true');
-    }
-  }, []);
-
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
-
+ const [showDialog, setShowDialog] = useState(true);
   return (
     <html lang="en">
       <head>
@@ -85,24 +70,35 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </head>
-               {showPopup && (
-    <div className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50">
-      <div className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
-        <h4 className="mb-4">Welcome to CosmoSpeak</h4>
-        <p className="mb-8">
-          Explore the wonders of Space Engineering through A.I.
-        </p>
-        <button
-          className="p-3 px-6 bg-black text-red-700"
-          onClick={() => {
-            window.location.href = 'https://square.link/u/RmVSmy9L';
-          }}
+              {showDialog && (
+        <dialog
+          className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50"
         >
-          Start for Free
-        </button>
-      </div>
-    </div>
-  )} 
+          <article className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
+            <h4 className="mb-4">Welcome to CosmoSpeak</h4>
+
+            <p className="mb-8">
+              Explore the wonders of Space Engineering through A.I.
+            </p>
+
+            <ol className="list-decimal mx-6 pl-2 mb-8">
+              <li>Get started w/ a free trial!</li>  
+              
+            </ol>
+          
+            <button
+  className="p-3 px-6 bg-black text-red-700"
+  onClick={() => {
+    window.location.href = 'https://square.link/u/RmVSmy9L';
+    return false;
+  }}
+>
+  Start Today
+</button>
+
+          </article>
+        </dialog>
+      )} 
       
       <body>
   
