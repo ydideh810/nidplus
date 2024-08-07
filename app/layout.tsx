868659ffd4ff57e9360fd354900577577883163e
ydideh810 <yd.baller810@gmail.com>
2024-08-07 16:56:39 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,15 @@ import { getClientConfig } from "./config/client";
 import { useEffect, useState } from "react";
 import { metadata } from './metadata'
 import { Metadata } from 'next';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
 // Metadata export
 
 
@@ -27,6 +36,15 @@ const cspHeader = `
     upgrade-insecure-requests;
 `;
 
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
 // Main component
 export default function RootLayout({
 
@@ -34,21 +52,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-const [showPopup, setShowPopup] = useState(true);
-useEffect(() => {
-  // Optionally, you can use localStorage to manage the popup visibility
-  // Example: Check if the popup has been shown before
-  const hasShownPopup = localStorage.getItem('hasShownPopup');
-  if (hasShownPopup) {
-    setShowPopup(false);
-  } else {
-    localStorage.setItem('hasShownPopup', 'true');
-  }
-}, []);
+const [open, setOpen] = React.useState(false);
 
-const handleClosePopup = () => {
-  setShowPopup(false);
-};
+const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <html lang="en">
@@ -85,24 +96,53 @@ const handleClosePopup = () => {
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </head>
-             {showPopup && (
-    <div className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50">
-      <div className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
-        <h4 className="mb-4">Welcome to CosmoSpeak</h4>
-        <p className="mb-8">
-          Explore the wonders of Space Engineering through A.I.
-        </p>
-        <button
-          className="p-3 px-6 bg-black text-red-700"
-          onClick={() => {
-            window.location.href = 'https://square.link/u/RmVSmy9L';
+            <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open dialog
+      </Button>
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          Modal title
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={{
+            position: 'absolute',
+            right: 8,
+            top: 8,
+            color: (theme) => theme.palette.grey[500],
           }}
         >
-          Start for Free
-        </button>
-      </div>
-    </div>
-  )} 
+          <CloseIcon />
+        </IconButton>
+        <DialogContent dividers>
+          <Typography gutterBottom>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </Typography>
+          <Typography gutterBottom>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+          </Typography>
+          <Typography gutterBottom>
+            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+            magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+            ullamcorper nulla non metus auctor fringilla.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Save changes
+          </Button>
+        </DialogActions>
+      </BootstrapDialog>
+    </React.Fragment>
       
       <body>
   
