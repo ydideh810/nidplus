5699084883fd85a266b8608cbb419ce51ae973c9
ydideh810 <yd.baller810@gmail.com>
2024-08-06 21:54:06 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { type Metadata } from "next";
 export const metadata: Metadata = {
   title: "Niddam_AI",
   description:
-    "YouR ultimate private assistant.",
+    "Your ultimate private assistant.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -79,6 +79,35 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </head>
+       {showDialog && (
+        <dialog
+          className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50"
+        >
+          <article className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
+            <h4 className="mb-4">Welcome to CosmoSpeak</h4>
+
+            <p className="mb-8">
+              Explore the wonders of Space Engineering through A.I.
+            </p>
+
+            <ol className="list-decimal mx-6 pl-2 mb-8">
+              <li>Get started w/ a free trial!</li>  
+              
+            </ol>
+          
+            <button
+  className="p-3 px-6 bg-black text-red-700"
+  onClick={() => {
+    window.location.href = 'https://square.link/u/RmVSmy9L';
+    return false;
+  }}
+>
+  Start Today
+</button>
+
+          </article>
+        </dialog>
+      )} 
       <body>{children}</body>
     </html>
   );
