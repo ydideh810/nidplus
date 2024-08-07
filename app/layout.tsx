f5a43f7bcc122aafc2d6b84efdb873b2065a62c1
ydideh810 <yd.baller810@gmail.com>
2024-08-07 00:39:38 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,23 +9,7 @@ import { getClientConfig } from "./config/client";
 import { useState, useEffect } from "react";
 
 // Metadata export
-export const metadata: Metadata = {
-  title: "Niddam_AI",
-  description: "Your ultimate private assistant.",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "#fafafa" },
-    { media: "(prefers-color-scheme: dark)", color: "#9FA2B8" },
-  ],
-  appleWebApp: {
-    title: "Niddam_AI",
-    statusBarStyle: "default",
-  },
-};
+
 
 // Content Security Policy header
 const cspHeader = `
