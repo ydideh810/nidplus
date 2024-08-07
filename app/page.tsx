5a9905e153f1e156c945c3482b185efb0382da6f
ydideh810 <yd.baller810@gmail.com>
2024-08-07 00:37:51 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,6 @@ import { Home } from "./components/home";
 export default async function App() {
   const [showDialog, setShowDialog] = useState(true);
    
-  return <Home />;
+  return <Home />
 
+}
