41dfd3c74731d6fd426af44649c8a7d21c92fb1b
ydideh810 <yd.baller810@gmail.com>
2024-08-07 01:14:02 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,27 +86,37 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
-        {showPopup && (
-          <div className="popup-overlay">
-            <div className="popup-content">
-              <h4 className="popup-title">Welcome to CosmoSpeak</h4>
-              <p className="popup-message">
-                Explore the wonders of Space Engineering through A.I.
-              </p>
-              <button
-                className="popup-button"
-                onClick={() => {
-                  window.location.href = 'https://square.link/u/RmVSmy9L';
-                }}
-              >
-                Start Today
-              </button>
-              <button className="popup-close" onClick={handleClosePopup}>
-                Close
-              </button>
-            </div>
-          </div>
-        )}
+         
+      {showDialog && (
+        <dialog
+          className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50"
+        >
+          <article className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
+            <h4 className="mb-4">Welcome to CosmoSpeak</h4>
+
+            <p className="mb-8">
+              Explore the wonders of Space Engineering through A.I.
+            </p>
+
+            <ol className="list-decimal mx-6 pl-2 mb-8">
+              <li>Get started w/ a free trial!</li>  
+              
+            </ol>
+          
+            <button
+  className="p-3 px-6 bg-black text-red-700"
+  onClick={() => {
+    window.location.href = 'https://square.link/u/RmVSmy9L';
+    return false;
+  }}
+>
+  Start Today
+</button>
+
+          </article>
+        </dialog>
+      )} 
+      
         {children}
       </body>
     </html>
