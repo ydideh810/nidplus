9bf7be078227023908f38b38f02bce9e074f665f
ydideh810 <yd.baller810@gmail.com>
2024-08-08 00:08:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,15 +32,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [open, setOpen] = useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+const [showDialog, setShowDialog] = useState(true);
 
   return (
     <html lang="en">
@@ -77,6 +69,31 @@ export default function RootLayout({
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </head>
+         {showDialog && (
+        <dialog className="fixed w-screen h-screen bg-black bg-opacity-50 inset-0 flex justify-center items-center z-50">
+          <article className="content-container bg-white p-8 overflow-y-scroll max-h-screen">
+            <h4 className="mb-4">Welcome to CosmoSpeak</h4>
+            <p className="mb-8">Explore the wonders of Space Engineering through A.I.</p>
+            <ol className="list-decimal mx-6 pl-2 mb-8">
+              <li>Get started w/ a free trial!</li>
+            </ol>
+            <button
+              className="p-3 px-6 bg-black text-red-700"
+              onClick={() => {
+                window.location.href = 'https://square.link/u/RmVSmy9L';
+              }}
+            >
+              Start Today
+            </button>
+            <button
+              className="p-3 px-6 bg-gray-300"
+              onClick={handleClose}
+            >
+              Close
+            </button>
+          </article>
+        </dialog>
+      )}
       <body>
       
         {children}
