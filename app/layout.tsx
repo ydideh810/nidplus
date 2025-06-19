972db1f680b9a056dc182d0551bfc8fbbfed4332
ydideh810 <yd.baller810@gmail.com>
2025-06-19 13:40:45 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,13 +66,13 @@ export default function RootLayout({
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/neu.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/neu.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#062578" />
